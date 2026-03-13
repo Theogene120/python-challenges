b76473a07a67621f29093265e813042e1d8a5a0f
Theogene <igiranezatheogene4@gmail.com>
2026-03-13 14:30:45 +0200

    updated move zeros

--- move.py
+++ move.py
@@ -1,25 +1,22 @@
-# def moveZeroes(nums):
-#     count = 0
-#     for num in nums:
-#         if num == 0:
-#             #del nums[idx]
-#             nums.remove(num)
-#             count += 1
-            
-#         for _ in range(count):
-#             nums.append(0)
 
-#     return nums
-
-# print(moveZeroes([0,1,0,3,12]))
-# print(moveZeroes([0,0,1]))
 
 def moveZeroes(nums):
-    for num in nums:
-        if num == 0:
-            nums.remove(num)
-            nums.append(num)
+    n = len(nums)
+    left = 0
+    right = 1
+
+    while right < n:
+        if nums[left] == 0 and nums[right] != 0:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right += 1
+        elif nums[left] != 0:
+            left += 1
+            right = left + 1
+        else:
+            right += 1
     return nums
 
 print(moveZeroes([0,1,0,3,12]))
 print(moveZeroes([0,0,1]))
+print(moveZeroes([1,0,1]))
