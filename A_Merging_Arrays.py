0ade774f0b631307dcffdc76802ca1f6ef88f4f0
Theogene <igiranezatheogene4@gmail.com>
2026-04-17 10:28:47 +0200

    Marging arrays

---
+++ A_Merging_Arrays.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+result = []
+i, j = 0, 0
+
+while i < n and j < m:
+    if a[i] <= b[j]:
+        result.append(a[i])
+        i += 1
+    else:
+        result.append(b[j])
+        j += 1
+
+while i < n:
+    result.append(a[i])
+    i += 1
+while j < m:
+    result.append(b[j])
+    j += 1
+
+print(*result)
